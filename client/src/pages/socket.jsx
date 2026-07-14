546da8488f7e427dc18fddd80478dddaf140b5a3
moses-lal <moseslal2692@gmail.com>
2026-07-14 16:32:10 +0530

    url add

--- client/src/pages/socket.jsx
+++ client/src/pages/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://live-event-manager.onrender.com", {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 5,
